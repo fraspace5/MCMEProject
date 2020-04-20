3def3dac90b7abedce9df981e01f07a36e7e9e56
fraspace5 <simonagottardi@192.168.1.20>
2020-04-20 21:08:34 +0200

    Bug fixes and v 3.7

--- src/main/java/com/mcme/mcmeproject/Mcproject.java
+++ src/main/java/com/mcme/mcmeproject/Mcproject.java
@@ -473,6 +473,8 @@ public class Mcproject extends JavaPlugin implements Listener, PluginMessageList
         
     }
     
+    
+    
     public Set<String> getProjectNames() {
         return PluginData.getProjectsAll().keySet();
     }
